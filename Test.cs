58d854fac0a3dc05721db74681930858dfab6b65
bottle2 <bento.schirmer@gmail.com>
2024-11-14 18:29:55 -0300

    Gera classes e indivíduos RDF com reflexão

--- Test.cs
+++ Test.cs
@@ -1,15 +1,64 @@
-using System;
+using System.Collections;
 using System.Reflection;
+using VDS.RDF;
+using VDS.RDF.Ontology;
+using VDS.RDF.Writing;
 
 internal struct OwlExportAttribute { }
 
 internal class Program
 {
-    static void Main(string[] _)
+    private static OntologyGraph g;
+    static void Main()
     {
-        var a = new PizzaMushroom();
-
         Console.WriteLine("Hello, world!");
+
+        g = new OntologyGraph(UriFactory.Create("http://pizza.com"));
+        g.NamespaceMap.AddNamespace("pizza", UriFactory.Create("http://pizza.com"));
+
+        _ = Traverse(new PizzaMushroom());
+        _ = Traverse(new Margherita());
+
+        new RdfXmlWriter().Save(g, "pizza.rdf");
+    }
+
+    static Individual Traverse(object any)
+    {
+        Type leType = any.GetType();
+
+        OntologyClass previous = g.CreateOntologyClass(g.CreateUriNode($"pizza:{leType.Name}"));
+        var individual = g.CreateIndividual(g.CreateUriNode($"pizza:{leType.Name}Individual"), previous.Resource);
+
+        foreach (var hey in leType.FindMembers(MemberTypes.Field, BindingFlags.Instance | BindingFlags.NonPublic, (m, fc) => true, null))
+        {
+            Console.WriteLine(hey);
+            var valor = ((FieldInfo)hey).GetValue(any);
+            if (valor!.GetType().IsAssignableTo(typeof(IEnumerable)))
+            {
+                Console.WriteLine("is some collection");
+                foreach (object obj in (IEnumerable)valor)
+                {
+                    // TODO
+                    // Precisa criar uma propriedade, aí faz um assert que não é assert entre dois
+                    // individuos.
+
+                    //Individual other = Traverse(obj);
+                    //var prop = g.CreateOntologyProperty(g.CreateUriNode($"pizza:{hey.Name}"));
+                    //prop.add
+                    //g.Assert(individual.Resource, prop.Resource, other.Resource);
+                    //individual.AddResourceProperty($"pizza:{hey.Name}", other.Resource, true);
+                }
+            }
+        }
+
+        for (Type current = leType.BaseType!; current != null && current != typeof(object); current = current.BaseType!)
+        {
+            OntologyClass lala = g.CreateOntologyClass(g.CreateUriNode($"pizza:{current.Name}"));
+            previous.AddSuperClass(lala);
+            previous = lala;
+        }
+
+        return individual;
     }
 }
 
