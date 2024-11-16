791b49e387864432e0f95cbc550ed1848749e236
Alexandre Chagas Brites <acbrites@inf.ufsm.br>
2024-11-16 19:40:12 -0300

    Move código da reflexão para uma classe separada

---
+++ OwlGenerator.cs
@@ -0,0 +1,191 @@
+﻿using System.Collections;
+using System.Reflection;
+
+internal struct OwlExportAttribute { }
+
+internal class OwlGenerator
+{
+    internal struct OwlProperty
+    {
+        internal Type domain;
+        internal string relation;
+        internal Type range;
+
+        internal OwlProperty(Type domain, string relation, Type range)
+        {
+            this.domain = domain;
+            this.relation = relation;
+            this.range = range;
+        }
+    }
+
+    internal struct OwlFact
+    {
+        internal string relation;
+        internal string individual;
+
+        internal OwlFact(string relation, string individual)
+        {
+            this.relation = relation;
+            this.individual = individual;
+        }
+    }
+
+    internal class OwlIndividual
+    {
+        internal List<string> aliases = [];
+        internal List<OwlFact> facts = [];
+
+        internal string name => aliases[0];
+
+        internal OwlIndividual(string name)
+        {
+            this.aliases = [name];
+        }
+    }
+
+    // TODO Devia ser um URI ou um (uuuuuuuuuurgh) "IRI".
+    internal string @namespace = "http://pizza.com";
+
+    // TODO MemberInfo não é imutável, por causa do contexto. Não deveríamos estar usando tuplas...
+    internal HashSet<OwlProperty> properties = [];
+
+    // TODO Deveria botar ValueType pra excluir
+    // mais tipos, mas talvez devesse excluir um
+    // monte de tipos que o usuário poderia estender?
+    // Ou talvez o usuário devesse colocar um atributo
+    // na classe pai que deve "parar"...
+    internal HashSet<Type> classes = [typeof(object), null];
+
+    internal Dictionary<object, OwlIndividual> individuals = [];
+    internal HashSet<string> allIndividualNames = [];
+
+    internal OwlGenerator(string @namespace)
+    {
+        this.@namespace = @namespace;
+    }
+
+    internal string AddIndividual(object obj)
+    {
+        return AddIndividual(obj, obj.GetType().Name);
+    }
+
+    internal string AddIndividual(object obj, string name)
+    {
+        OwlIndividual? individual;
+        if (individuals.TryGetValue(obj, out individual))
+        {
+            if (individual.aliases.Contains(name))
+                return name;
+
+            individual.aliases.Add(UniqueIndividualName(name));
+            return individual.name;
+        }
+
+        AddClass(obj.GetType());
+
+        individual = new OwlIndividual(UniqueIndividualName(name));
+        individuals.Add(obj, individual);
+
+        foreach (var memberInfo in obj.GetType().FindMembers(MemberTypes.Field, BindingFlags.Instance | BindingFlags.NonPublic, (m, fc) => true, null))
+        {
+            if (memberInfo is FieldInfo fieldInfo)
+            {
+                // XXX Ontologias não tem listas
+                //     Então repetimos várias vezes a mesma relação!
+                var fieldValue = fieldInfo.GetValue(obj);
+                if (fieldValue!.GetType().IsAssignableTo(typeof(IEnumerable)))
+                {
+                    foreach (object fieldIndividual in (IEnumerable)fieldValue)
+                        individual.facts.Add(new OwlFact($"has{fieldInfo.Name}", AddIndividual(fieldIndividual)));
+                }
+            }
+        }
+
+        return individual.name;
+    }
+
+    internal void AddClass(Type @class)
+    {
+        if (classes.Contains(@class))
+            return;
+
+        classes.Add(@class);
+
+        // TODO Deveríamos buscar propriedades também, além de atributos
+        // TODO Reconsiderar outros BindingFlag, tipo pegar os públicos?
+        foreach (var memberInfo in @class.FindMembers(MemberTypes.Field, BindingFlags.Instance | BindingFlags.NonPublic | BindingFlags.DeclaredOnly, (m, fc) => true, null))
+        {
+            if (memberInfo is FieldInfo fieldInfo)
+            {
+                Type domain = @class;
+                string relation = memberInfo.Name;
+                // TODO Nem sei se isso funciona! Só tava preocupado com Topping[] quando escrevi.
+                // TODO Se o tipo for não gerenciado como inteiro ou string, deveria ser um "DataProperty" ao invés de "ObjectPropert" no Manchester gerado
+                // TODO Ver se funciona com coisas que não são descendentes de IEnumerable, como um atributo comum ou um Dictionary
+                Type range = fieldInfo.FieldType.IsAssignableTo(typeof(IEnumerable)) ? fieldInfo.FieldType.GetElementType()! : fieldInfo.FieldType!;
+                properties.Add(new OwlProperty(domain, relation, range));
+            }
+        }
+
+        AddClass(@class.BaseType!);
+    }
+
+    internal void Render(string filename)
+    {
+        // TODO E o caminho pra salvar? Atualmente vai pra pasta do projeto/bin/Debug/net8.0/
+        using StreamWriter sw = new($"{filename}.omn", false, System.Text.Encoding.UTF8);
+        sw.WriteLine($"Prefix: : <{@namespace}/>");
+        sw.WriteLine();
+        sw.WriteLine($"Ontology: <{@namespace}>");
+        sw.WriteLine();
+
+        foreach (OwlProperty property in properties)
+        {
+            // TODO Essa coisa de has{} e is{]Of é claramente uma gambiarra
+            //      Criar uma anotação pro usuário dizer o nome das relações inversas, caso sejam inversas sequer!
+            sw.WriteLine($"ObjectProperty: has{property.relation}");
+            sw.WriteLine($"  Domain:    {property.domain.Name}");
+            sw.WriteLine($"  Range:     {property.range.Name}");
+            sw.WriteLine($"  InverseOf: is{property.relation}Of");
+            sw.WriteLine();
+            sw.WriteLine($"ObjectProperty: is{property.relation}Of");
+            sw.WriteLine();
+        }
+
+        foreach (Type @class in classes)
+        {
+            if (@class == null || @class == typeof(object))
+                continue;
+            sw.WriteLine($"Class: {@class.Name}");
+            if (@class.BaseType != typeof(object))
+                sw.WriteLine($"  SubClassOf: {@class.BaseType!.Name}");
+            sw.WriteLine();
+        }
+
+        foreach ((object obj, OwlIndividual individual) in individuals)
+        {
+            sw.WriteLine($"Individual: {individual.name}");
+            sw.WriteLine($"  Types: {obj.GetType().Name}");
+            foreach (OwlFact fact in individual.facts)
+                sw.WriteLine($"  Facts: {fact.relation} {fact.individual}");
+            sw.WriteLine();
+            for (int i = 1; i < individual.aliases.Count; i++)
+            {
+                sw.WriteLine($"Individual: {individual.aliases[i]}");
+                sw.WriteLine($"  SameAs: {individual.name}");
+                sw.WriteLine();
+            }
+        }
+    }
+
+    private string UniqueIndividualName(string name)
+    {
+        while (allIndividualNames.Contains(name))
+        {
+            name += " - (Copia)"; // Toma Windows venceu.
+        }
+
+        return name;
+    }
+}
--- Test.cs
+++ Test.cs
@@ -1,135 +1,17 @@
-using System.Collections;
-using System.Reflection;
-
 // XXX Coisas parecidas?
 // https://github.com/dotnetrdf/dotnetrdf/discussions/594
 // - https://github.com/giacomociti/iride
 // - https://github.com/EcoStruxure/OLGA
 // - https://github.com/ukparliament/Mapping
 
-internal struct OwlExportAttribute { }
-
 internal class Program
 {
     static void Main()
     {
-        // TODO Devia ser um URI ou um (uuuuuuuuuurgh) "IRI".
-        string @namespace = "http://pizza.com";
-        // TODO E o caminho pra salvar? Atualmente vai pra pasta do projeto/bin/Debug/net8.0/
-        string filename = "pizza";
-
-        // TODO Deveria ir para uma classe que cuida da reflexão
-        using StreamWriter sw = new($"{filename}.omn", false, System.Text.Encoding.UTF8);
-        sw.WriteLine($"Prefix: : <{@namespace}/>");
-        sw.WriteLine($"Ontology: <{@namespace}>");
-
-        // TODO Arrancar fora esssas coleções e botar dentro de uma classe de instância.
-        Dictionary<object, List<string>> individuals = [];
-        HashSet<string> allIndividualNames = [];
-        HashSet<Type?> classes = [typeof(object), null]; // TODO Deveria botar ValueType pra excluir
-                                                         // mais tipos, mas talvez devesse excluir um
-                                                         // monte de tipos que o usuário poderia estender?
-                                                         // Ou talvez o usuário devesse colocar um atributo
-                                                         // na classe pai que deve "parar"...
-
-        // TODO MemberInfo não é imutável, por causa do contexto. Não deveríamos estar usando tuplas...
-        HashSet<(Type, string, Type)> properties = [];
-
-        Traverse(new PizzaMushroom(), "mushroomPizza");
-        Traverse(new Margherita(), "margheritaPizza");
-
-        // TODO Deveria ir para uma classe que cuida da reflexão
-        string Traverse(object o, string name)
-        {
-            if (individuals.TryGetValue(o, out List<string> aliases))
-            {
-                if (aliases.Contains(name))
-                    return name;
-                else
-                {
-                    aliases.Add(UniqueIndividualName(name));
-                    sw.WriteLine($"Individual: {aliases[^1]}");
-                    sw.WriteLine($"  SameAs: {aliases[0]}");
-                    return aliases[0];
-                }
-            }
-            else
-            {
-                for (
-                    Type? current = o.GetType(), parent = current.BaseType;
-                    !classes.Contains(current);
-                    classes.Add(current), current = parent, parent = parent.BaseType
-                ) {
-                    sw.WriteLine($"Class: {current.Name}");
-                    if (parent != typeof(object))
-                        sw.WriteLine($"  SubClassOf: {parent!.Name}");
-
-                    // TODO Deveríamos buscar propriedades também, além de atributos
-                    // TODO Reconsiderar outros BindingFlag, tipo pegar os públicos?
-                    foreach (var p in current.FindMembers(MemberTypes.Field, BindingFlags.Instance | BindingFlags.NonPublic | BindingFlags.DeclaredOnly, (m, fc) => true, null))
-                    {
-                        if (p is FieldInfo fi)
-                        {
-                            Type domain = current;
-                            string relation = p.Name;
-                            // TODO Nem sei se isso funciona! Só tava preocupado com Topping[] quando escrevi.
-                            // TODO Se o tipo for não gerenciado como inteiro ou string, deveria ser um "DataProperty" ao invés de "ObjectPropert" no Manchester gerado
-                            // TODO Ver se funciona com coisas que não são descendentes de IEnumerable, como um atributo comum ou um Dictionary
-                            Type range = fi.FieldType.IsAssignableTo(typeof(IEnumerable)) ? fi.FieldType.GetElementType() : fi.FieldType;
-
-                            if (properties.Add((domain, relation, range)))
-                            {
-                                // TODO Essa coisa de has{} e is{]Of é claramente uma gambiarra
-                                //      Criar uma anotação pro usuário dizer o nome das relações inversas, caso sejam inversas sequer!
-                                sw.WriteLine($"ObjectProperty: has{p.Name}");
-                                sw.WriteLine($"  Domain:    {domain.Name}");
-                                sw.WriteLine($"  Range:     {range.Name}");
-                                sw.WriteLine($"  InverseOf: is{p.Name}Of");
-                                sw.WriteLine($"ObjectProperty: is{p.Name}Of");
-                            }
-                        }
-                    }
-                }
-
-                name = UniqueIndividualName(name);
-                individuals.Add(o, [name]);
-
-                // XXX Importante isso aqui por causa da recursão!
-                List<(string, string)> postpone = [];
-
-                foreach (var p in o.GetType().FindMembers(MemberTypes.Field, BindingFlags.Instance | BindingFlags.NonPublic, (m, fc) => true, null))
-                {
-                    if (p is FieldInfo fi)
-                    {
-                        // XXX Ontologias não tem listas
-                        //     Então repetimos várias vezes a mesma relação!
-                        var valor = fi.GetValue(o);
-                        if (valor!.GetType().IsAssignableTo(typeof(IEnumerable)))
-                        {
-                            foreach (object obj in (IEnumerable)valor)
-                                postpone.Add(("has" + fi.Name, Traverse(obj, obj.GetType().Name)));
-                        }
-                    }
-                }
-
-                sw.WriteLine($"Individual: {name}");
-                sw.WriteLine($"  Types: {o.GetType().Name}");
-                foreach ((string relation, string indi) in postpone)
-                    sw.WriteLine($"  Facts: {relation} {indi}");
-
-                return name;
-            }
-        }
-
-        string UniqueIndividualName(string name)
-        {
-            while (allIndividualNames.Contains(name))
-            {
-                name += " - (Copia)"; // Toma Windows venceu.
-            }
-
-            return name;
-        }
+        OwlGenerator owl = new OwlGenerator("http://pizza.com");
+        owl.AddIndividual(new PizzaMushroom(), "mushroomPizza");
+        owl.AddIndividual(new Margherita(), "margheritaPizza");
+        owl.Render("pizza");
     }
 }
 
