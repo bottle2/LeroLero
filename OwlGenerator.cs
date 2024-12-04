8adbdfc72960cbfaa5c4a12431760ba6531a69b4
Alexandre Chagas Brites <acbrites@inf.ufsm.br>
2024-12-04 00:06:23 -0300

    Adiciona SubClassOf de DataProperty e ObjectProperty as classes. Adiciona structs wrappers com getters para que o StringTemplate possa usar

--- OwlGenerator.cs
+++ OwlGenerator.cs
@@ -8,40 +8,85 @@ using Antlr4.StringTemplate.Misc;
 
 internal class OwlGenerator
 {
+    internal struct OwlValue
+    {
+        public object value;
+
+        public readonly bool IsString => value is string;
+        public readonly bool IsEnum => value.GetType().IsEnum;
+        public readonly bool IsBool => value is bool;
+        public readonly bool IsFloat => value is float;
+
+        private static NumberFormatInfo numberFormatInfo = new() { NumberDecimalSeparator = "." };
+
+        internal OwlValue(object value)
+        {
+            this.value = value;
+        }
+
+        public override readonly string ToString()
+        {
+            if (value is float f)
+                return f.ToString(numberFormatInfo);
+            else if (value is double d)
+                return d.ToString(numberFormatInfo);
+            else if (value is decimal dec)
+                return dec.ToString(numberFormatInfo);
+            return value.ToString();
+        }
+    }
+
+    internal struct OwlType
+    {
+        public Type type;
+
+        public readonly bool IsData => type.IsPrimitive || type.IsEnum || type == typeof(string);
+        public readonly bool IsEnum => type.IsEnum;
+        public readonly string[] Enumerators => type.GetEnumNames();
+
+        internal OwlType(Type type)
+        {
+            this.type = type;
+        }
+
+        public override string ToString() => type.Name;
+    }
+
     internal struct OwlProperty
     {
-        public Type domain;
+        public OwlType domain;
         public string relation;
-        public Type range;
-
-        public readonly bool IsData => range.IsPrimitive || range == typeof(string);
-        public readonly bool IsEnum => range.IsEnum;
-        public readonly string[] Enumerators => range.GetEnumNames();
+        public OwlType range;
 
         internal OwlProperty(Type domain, string relation, Type range)
         {
-            this.domain = domain;
+            this.domain = new OwlType(domain);
             this.relation = relation;
-            this.range = range;
+            this.range = new OwlType(range);
         }
+
+        public override string ToString() => relation;
     }
 
     internal struct OwlFact // TODO Podia ser um record
     {
         public string relation;
-        public string individual;
+        public OwlValue value;
 
-        internal OwlFact(string relation, string individual)
+        internal OwlFact(string relation, object value)
         {
             this.relation = relation;
-            this.individual = individual;
+            this.value = new OwlValue(value);
         }
+
+        public override string ToString() => relation;
     }
 
     internal class OwlClass
     {
         public Type Type;
         public OwlClass? SuperClass;
+        public HashSet<OwlProperty> Properties = [];
 
         public string? Comment => Attribute.GetCustomAttributes(Type)
             .OfType<OwlCommentAttribute>()
@@ -78,7 +123,7 @@ internal class OwlGenerator
     }
 
     // TODO Devia ser um URI ou um (uuuuuuuuuurgh) "IRI".
-    internal string @namespace = "http://pizza.com";
+    internal string ontology = "http://pizza.com";
     internal string comment;
 
     // TODO MemberInfo não é imutável, por causa do contexto. Não deveríamos estar usando tuplas...
@@ -94,13 +139,15 @@ internal class OwlGenerator
     internal Dictionary<object, OwlIndividual> individuals = [];
     internal HashSet<string> allIndividualNames = [];
 
-    internal OwlGenerator(string @namespace, string comment = "")
+    internal Regex backingFieldRegex = new(@"<([^>]+)>k__BackingField");
+
+    internal OwlGenerator(string ontology, string comment = "")
     {
-        this.@namespace = @namespace;
+        this.ontology = ontology;
         this.comment = comment;
     }
 
-    internal string AddIndividual(object obj)
+    internal OwlIndividual AddIndividual(object obj)
     {
         // TODO Search for name using reflection.
         return AddIndividual(obj, FindName(obj)?.Replace(' ', '+') ?? obj.GetType().Name);
@@ -122,16 +169,16 @@ internal class OwlGenerator
         }
     }
 
-    internal string AddIndividual(object obj, string name)
+    internal OwlIndividual AddIndividual(object obj, string name)
     {
         OwlIndividual? individual;
         if (individuals.TryGetValue(obj, out individual))
         {
             if (individual.aliases.Contains(name))
-                return name;
+                return individual;
 
             individual.aliases.Add(UniqueIndividualName(name));
-            return individual.name;
+            return individual;
         }
 
         AddClass(obj.GetType());
@@ -143,50 +190,22 @@ internal class OwlGenerator
         {
             if (memberInfo is FieldInfo fieldInfo)
             {
-                // TODO Código duplicado
-                string nameson = fieldInfo.Name;
-                Regex match = new(@"<([^>]+)>k__BackingField");
-                Match match2 = match.Match(nameson);
-                if (match2.Success)
-                    nameson = match2.Groups[1].Value;
+                string relation = GetPropertyName(fieldInfo.Name);
 
-                if (fieldInfo.FieldType.IsPrimitive || fieldInfo.FieldType == typeof(string))
+                var fieldValue = fieldInfo.GetValue(obj)!;
+                if (fieldInfo.FieldType != typeof(string) && fieldInfo.FieldType.IsAssignableTo(typeof(IEnumerable)))
                 {
-                    string? tits = null;
-                    if (fieldInfo.FieldType == typeof(string))
-                        tits = $"\"{(string)fieldInfo.GetValue(obj)!}\"";
-                    else if (fieldInfo.FieldType == typeof(bool))
-                        tits = (bool)fieldInfo.GetValue(obj)! ? "true" : "false";
-                    else if (fieldInfo.FieldType == typeof(float) || fieldInfo.FieldType == typeof(double) || fieldInfo.FieldType == typeof(decimal))
-                    {
-                        NumberFormatInfo nfi = new();
-                        nfi.NumberDecimalSeparator = ".";
-                        tits = (string)fieldInfo.FieldType.InvokeMember("ToString", BindingFlags.InvokeMethod, null, fieldInfo.GetValue(obj), new object[] { nfi })!;
-                        // TODO Acabou o clean code.
-                        if (fieldInfo.FieldType == typeof(float))
-                            tits += "f";
-                    }
-                    else
-                        tits = fieldInfo.GetValue(obj)!.ToString();
-                    individual.facts.Add(new OwlFact(nameson, tits!));
+                    foreach (object fieldIndividual in (IEnumerable)fieldValue)
+                        individual.facts.Add(new OwlFact(relation, AddIndividual(fieldIndividual)));
                 }
-                else if (fieldInfo.FieldType.IsEnum)
-                    individual.facts.Add(new OwlFact(nameson, $"\"{fieldInfo.GetValue(obj)!}\""));
                 else
                 {
-                    var fieldValue = fieldInfo.GetValue(obj);
-                    // XXX Ontologias não tem listas
-                    //     Então repetimos várias vezes a mesma relação!
-                    if (fieldValue!.GetType().IsAssignableTo(typeof(IEnumerable)))
-                    {
-                        foreach (object fieldIndividual in (IEnumerable)fieldValue)
-                            individual.facts.Add(new OwlFact($"has{fieldInfo.Name}", AddIndividual(fieldIndividual)));
-                    }
+                    individual.facts.Add(new OwlFact(relation, fieldValue));
                 }
             }
         }
 
-        return individual.name;
+        return individual;
     }
 
     internal OwlClass? AddClass(Type? type)
@@ -208,23 +227,17 @@ internal class OwlGenerator
             if (memberInfo is FieldInfo fieldInfo)
             {
                 Type domain = type;
-
-                // TODO Código duplicado
-                string nameson = fieldInfo.Name;
-                Regex match = new(@"<([^>]+)>k__BackingField");
-                Match match2 = match.Match(nameson);
-                if (match2.Success)
-                    nameson = match2.Groups[1].Value;
-
-                // TODO Nem sei se isso funciona! Só tava preocupado com Topping[] quando escrevi.
-                // TODO Ver se funciona com coisas que não são descendentes de IEnumerable, como um Dictionary
+                string relation = GetPropertyName(fieldInfo.Name);
                 Type range = fieldInfo.FieldType;
 
                 if (!range.IsPrimitive && range != typeof(string)) // TODO Código duplicado
                 {
                     range = fieldInfo.FieldType.IsAssignableTo(typeof(IEnumerable)) ? fieldInfo.FieldType.GetElementType()! : fieldInfo.FieldType!;
                 }
-                properties.Add(new OwlProperty(domain, nameson, range));
+
+                OwlProperty property = new OwlProperty(domain, relation, range);
+                klass.Properties.Add(property);
+                properties.Add(property);
             }
         }
 
@@ -237,7 +250,7 @@ internal class OwlGenerator
         group.Listener = new StringTemplateErrorListener();
 
         Template template = group.GetInstanceOf("decl");
-        template.Add("namespace", @namespace);
+        template.Add("ontology", ontology);
         template.Add("comment", comment);
         template.Add("properties", properties);
         template.Add("classes", classes.Values);
@@ -261,6 +274,12 @@ internal class OwlGenerator
             }
         }
         throw new ArgumentException("Quantidade de indivíduos para classe excedida!", nameof(name));
+    }
+
+    private string GetPropertyName(string name)
+    {
+        Match match = backingFieldRegex.Match(name);
+        return match.Success ? match.Groups[1].Value : name;
     }
 
     internal sealed class StringTemplateErrorListener : ITemplateErrorListener
